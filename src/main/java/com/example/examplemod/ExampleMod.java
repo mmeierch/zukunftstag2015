b8ae4ce623b2daf774dd9950baaa06bc7e4df342
Zisch <zisch@localhost>
2015-11-07 13:51:46 +0100

    trying to fix Eclipse problem

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,7 +10,7 @@ public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
@@ -18,3 +18,4 @@ public class ExampleMod
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
 }
+
